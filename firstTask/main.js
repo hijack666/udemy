66bed708de8ad0cf3fa2000167638da04f097e73
hiJack <pushmeone@mail.ru>
2020-03-30 14:24:56 +0700

    add questions

--- firstTask/main.js
+++ firstTask/main.js
@@ -14,7 +14,10 @@ var appData = {
 
 var qOne = prompt("Введите обязательную статью расходов в этом месяце");
 var qTwo = prompt("Во сколько обойдется?");
+var qThree = prompt("Введите обязательную статью расходов в этом месяце");
+var qFour = prompt("Во сколько обойдется?");
 
 appData.expenses.qOne = qTwo;
+appData.expenses.qThree = qFour;
 
 alert(appData.budget / 30);
