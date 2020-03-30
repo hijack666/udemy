b35b9b9a44a96d11eb80ae8ebd5f8d7cf1ca3328
hiJack <pushmeone@mail.ru>
2020-03-30 13:13:22 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var a=5;
+console.log(a);
+
+// Типы данных 
+var number = 5;
+var string = 'Hello!';
+var sym = Symbol();
+var boolean = true;
+// null; 
+// undefined;
+var obj = {};
+
+// МАССИВ - частный случай объекта
